747dd569f83183f46bf630423d364eb9f5c52558
Americo Albuquerque <chainsword@gmail.com>
2014-02-24 00:49:32 +0100

    Added edit test case functionality Fixed regexp functionality Improved regexp functionality Inproved storage serialization Improved UI

--- js/app.js
+++ js/app.js
@@ -3,40 +3,6 @@
  */
 
 (function() {
-	function ClassNameModel() {
-
-	}
-	ClassNameModel.prototype = {
-			init: function(element, valueAccessor, allBindings, bindingContext) {
-				//console.log("[init]", "className:", arguments);
-				var value = valueAccessor();
-				var valueUnwrapped = ko.unwrap(value);
-				$(element).addClass(valueUnwrapped.toString());
-			},
-			update: function(element, valueAccessor, allBindings, bindingContext) {
-				//console.log("[update]", "className:", arguments);
-				var value = valueAccessor();
-				var valueUnwrapped = ko.unwrap(value);
-				$(element).addClass(valueUnwrapped.toString());
-			}
-	};
-	function HtmlModel() {
-
-	}
-	HtmlModel.prototype = {
-			init: function(element, valueAccessor, allBindings, bindingContext) {
-				//console.log("[init]", "html:", arguments);
-				var value = valueAccessor();
-				var valueUnwrapped = ko.unwrap(value);
-				$(element).html(valueUnwrapped.toString());
-			},
-			update: function(element, valueAccessor, allBindings, bindingContext) {
-				//console.log("[update]", "html:", arguments);
-				var value = valueAccessor();
-				var valueUnwrapped = ko.unwrap(value);
-				$(element).html(valueUnwrapped.toString());
-			}
-	};
 	function RegExpModel() {
 		this._data = {};
 	}
@@ -45,26 +11,20 @@
 				console.log("[init]", "RegExp:", arguments);
 				var value = valueAccessor();
 				var valueUnwrapped = ko.unwrap(value);
-				//console.log("[init]", "RegExp:", element);
-				model.bindings.regexp._data[element] = {
-						value: valueUnwrapped,
-						element: $(element).html()
-				};
-			},
-			update: function(element, valueAccessor, allBindings, bindingContext) {
-				console.log("[update]", arguments);
-				var value = valueAccessor();
-				var valueUnwrapped = ko.unwrap(value);
-				var text = $(element).html();
+
 				var div = $(element);
-				div.empty();
+				
+				console.log("[init]", "data:", valueUnwrapped);
+				
+				var text = valueUnwrapped.value;
 				var start = 0;
 				//var text = model.bindings.regexp._data[element].element;
-				console.log("[update]", "text:", text);
-				text.replace(new RegExp(valueUnwrapped, "i"), function(match) {
-					console.log("[update]", "RegExp:", arguments);
+				console.log("[init]", "text:", text);
+				text.replace(new RegExp(valueUnwrapped.regexp, "ig"), function(match) {
 					var offset = arguments[arguments.length-2];
-					div.append(text.substr(start, offset));
+					console.log("[init]", "RegExp:", start, text.substr(start, offset-start), arguments);
+					
+					div.append(text.substr(start, offset-start));
 					var group = "";
 					for(var i=1; i<arguments.length-2; ++i) {
 						//if(arguments[i]) {
@@ -84,28 +44,22 @@
 				if(start<text.length) {
 					div.append(text.substr(start));
 				}
-			}
-	};
-	function RelModel() {
-
-	}
-	RelModel.prototype = {
-			init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-	            //console.log("[init]", "rel:", arguments);
-				var value = valueAccessor();
-				var valueUnwrapped = ko.unwrap(value);
-				$(element).attr('rel', valueUnwrapped);
-	        },
+				div.addClass(start>0?"true":"false");
+			},
 			update: function(element, valueAccessor, allBindings, bindingContext) {
-				//console.log("[update]", "rel:", arguments);
+				console.log("[update]", "RegExp:", arguments);
 			}
 	};
+	
+	function TestCaseModel(data, id) {
+		this.data = ko.observable(data);
+		this.id = id || Utils.guid();
+	}
 
 	function ViewModel() {
 		this._data = {
 				regexp: "",
 				testcase: "",
-				global_id: 0,
 				testCases: [],
 				results: []
 		};
@@ -115,47 +69,58 @@
 					JSON.parse(localStorage.data)
 			);
 		}
-		this._selected = null;
-		function observable(obj, data) {
-			_.each(data, function(value, key) {
-				if(Utils.type(value)=="array") {
-					obj[key] = ko.observableArray(value);
-					return;
-				}
-				if(Utils.type(value)=="obj") {
-					obj[key] = {};
-					observable(obj[key], value);
-					return;
-				}
-				obj[key] = ko.observable(value);
-			});
-		}
 		console.log("[ViewModel]", this._data);
-		observable(this, this._data);
 		
-		this.bindings = {};
-		this.bindings.className = ko.bindingHandlers.className = new ClassNameModel();
-		this.bindings.html = ko.bindingHandlers.html = new HtmlModel();
-		this.bindings.regexp = ko.bindingHandlers.RegExp = new RegExpModel();
-		//this.bindings.click = ko.bindingHandlers.click = new ClickModel();
+		this.regexp = ko.observable(this._data.regexp);
+		this.testcase = ko.observable();
+		this.testCases = ko.observableArray();
+		this.results = ko.observableArray();
+		_.each(this._data.testCases, function(test) {
+			this.push(new TestCaseModel(test.data, test.id));
+		}, this.testCases);
+		
+		ko.bindingHandlers.RegExp = new RegExpModel();
 	}
 	ViewModel.prototype = {
+			unserialize: function() {
+				if(localStorage.data) {
+					Utils.merge(
+							this._data,
+							JSON.parse(localStorage.data)
+					);
+				}
+				console.log("[ViewModel]", this._data);
+				this.regexp = ko.observable(this._data.regexp);
+				this.testCases = ko.observableArray();
+				_.each(this._data.testCases, function(test) {
+					this.push(new TestCaseModel(test.data, test.id));
+				}, this.testCases);
+			},
+			serialize: function() {
+				console.log("[serialize]", this._data);
+				var data = {regexp:"", testCases:[]};
+				data.regexp = ko.unwrap(this.regexp());
+				var testcases = ko.unwrap(this.testCases);
+				_.each(testcases, function(item){
+					this.push({id:item.id, data:ko.unwrap(item.data())});
+				}, data.testCases);
+				console.log("[serialize]", data);
+				localStorage.data = JSON.stringify(data);
+			},
+			
 			runTestCases: function() {
-
-				var self = this;
-				var regexpText = ko.unwrap(this.regexp());
-				var regexp = regexpText;
+				var regexp = ko.unwrap(this.regexp());
 				var list = ko.unwrap(this.testCases());
-				console.log("[ViewModel::runTestCases]", this._data, this.regexp(), regexpText, list);
+				console.log("[ViewModel::runTestCases]", this._data, this.regexp(), regexp, list);
 				this.results.removeAll();
+				
 				_.each(list, function(testcase, key) {
 					console.log("[ViewModel::list.each]", arguments, testcase, key);
-					self.results.push({
-						test: new RegExp(regexp, "i").test(testcase.data),
+					this.push({
 						regexp: regexp,
-						value: testcase.data
+						value: ko.unwrap(testcase.data)
 					});
-				});
+				}, this.results);
 			},
 			addTestCase: function() {
 				$('.test.edit.case').show(function() {
@@ -170,10 +135,7 @@
 			},
 			saveTestCase: function() {
 				if(this.testcase()!="") {
-					this.testCases.push({
-						data: ko.utils.unwrapObservable(this.testcase),
-						id: this.nextId()
-					});
+					this.testCases.push(new TestCaseModel(ko.unwrap(this.testcase)));
 				}
 				$('.test.edit.case').hide(function() {
 					$('.test.edit.case input.edit').val("");
@@ -188,12 +150,12 @@
 			select: function(data) {
 				console.log("[ViewModel::select]", arguments);
 				if(model._selected) {
-					$('#editor'+model._selected.id).hide();
+					$('#editor'+model._selected.id).hide(100);
 				}
 				model._selected = data;
 				//$('#span'+data.id).fadeTo(0, 0);
 				//$('#span'+data.id).hide();
-				$('#editor'+data.id).show();
+				$('#editor'+data.id).show(100);
 				$('.test.cases.command .remove').removeAttr('disabled');
 			},
 			
@@ -202,6 +164,9 @@
 				$('#input'+data.id).show(function() {
 					$(this).focus();
 				});
+				$('#edit_panel_'+data.id).hide(function(){});
+				$('#confirm_panel_'+data.id).show(function(){});
+				$('div.test.cases div.test.case .selector').hide();
 			},
 			
 			remove: function(data) {
@@ -209,6 +174,24 @@
 				model.testCases.remove(data);
 				model._selected = null;
 				$('.test.cases.command .remove').attr("disabled", "disabled");
+			},
+			
+			save: function(data) {
+				$('#input'+data.id).hide(function() {});
+				$('#edit_panel_'+data.id).show(function(){});
+				$('#confirm_panel_'+data.id).hide(function(){});
+				$('div.test.cases div.test.case .selector').show();
+			},
+			
+			cancel: function(data) {
+				$('#input'+data.id).hide(function() {});
+				$('#edit_panel_'+data.id).show(function(){});
+				$('#confirm_panel_'+data.id).hide(function(){});
+				$('div.test.cases div.test.case .selector').show();
+			},
+			
+			itemChanged: function(data) {
+				console.log("[itemChanged]", arguments);
 			},
 			
 			nextId: function() {
@@ -225,12 +208,7 @@
 		console.log("window.load", sessionStorage, localStorage);
 	});
 	$(window).bind('beforeunload',function(){
-		model._data.regexp = ko.utils.unwrapObservable(model.regexp);
-		model._data.results = [];
-		localStorage.data = JSON.stringify(model._data);
-		return '';
-	});
-	$(window).unload(function() {
-		sessionStorage.data = JSON.stringify(model._data);
+		model.serialize();
+		return 'Serializing data';
 	});
 }).call(this);
